ce5c058d46abaa78c8a1d512d1d1c3ae0c7fa5c6
Khyber <ajbriquelet@gmail.com>
2024-01-19 15:26:52 -0600

    new branch with template for questions array and comments for event listener

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
-// OBJECT FULL OF QUESTIONS GOES HERE//
+//QUESTIONS GO HERE//
+const multiChoice = [ {
+qnumb:
+question:
+correct:
+choices: [
+
+]
+},
+{
+qnumb:
+question:
+correct:
+choices: [
+    
+]
+},
+{
+qnumb:
+question:
+correct:
+choices: [
+        
+]
+}
+]
+//END QUESTIONS//
+
+//startQuiz button selector//
+const startQuiz = document.getElementById('startQuiz');
+
+//ADD EVENT LISTENER WITH FUNCTION TO RUN WHEN USER STARTS QUIZ//
+
+
+
+
+
+
+
+
+
+
+
+
 
 
